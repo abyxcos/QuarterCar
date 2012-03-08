c94d54b893621e82c37ad09efad75322cd73e054
unknown <rainem1@.meng.lab.emc.com>
2012-03-08 18:42:51 -0500

    Missing semicolon in the spring matrix.

--- modelFull.m
+++ modelFull.m
@@ -129,7 +129,7 @@ function xdot = modelFull(t, x, p)
     k = [K11 K12 K13 K14 K15 K16 K17;
          K21 K22 K23 K24 K25 K26 K27;
          K31 K32 K33 K34 K35 K36 K37;
-         K41 K42 K43 K44 K45 K46 K47
+         K41 K42 K43 K44 K45 K46 K47;
          K51 K52 K53 K54 K55 K56 K57;
          K61 K62 K63 K64 K65 K66 K67;
          K71 K72 K73 K74 K75 K76 K77];

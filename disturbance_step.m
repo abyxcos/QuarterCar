46affb924d2ee3e9168eb773a930145e4287b12a
Aaron Fineman <abyxcos@gmail.com>
2012-03-08 22:25:37 -0500

    Initial attempt at adding a driving function back into the model

---
+++ disturbance_step.m
@@ -0,0 +1,10 @@
+% Ground distrubance(s)
+
+function y=disturbance_step(t)
+    global bump_height;
+    y=0;
+    
+    if(t>3.0)
+        y=bump_height;
+    end
+end
---
+++ init_globals.m
@@ -0,0 +1,10 @@
+% Init Car Parameters
+% This script initializes the global variables used in all the car
+% models.
+global g bump_height;
+
+% Gravity
+g = 9.81;
+
+% Height for disturbance function
+bump_height=0.02; % 2cm

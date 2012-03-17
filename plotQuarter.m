a2b3dc6f58e6b0ab4c1f3315c5cf7faaece28aba
unknown <rainem1@.meng.lab.emc.com>
2012-03-17 18:55:10 -0400

    Adding acceleration plots.

--- plotQuarter.m
+++ plotQuarter.m
@@ -5,22 +5,35 @@ function plotQuarter(t, x)
     init_globals_quarter;
     
     % Run the quarter car model
-    [t,x]=ode45(@(t,x) modelQuarter(t, x, q_car), [0 5], [0; 0; 0; 0]);
+    [t, x] = ode45(@(t, x) modelQuarter(t, x, q_car), [0 5], [0; 0; 0; 0]);
+
+    % Determine the accelerations based off the returned velocities
+    F1 = diff(x(:, 3))./diff(t);
+    F2 = diff(x(:, 4))./diff(t);
+    tt = 0:(t(end) / (length(F1) - 1)):t(end);
     
     % Generate the plots
-    subplot(1,4,1);
-    plot(t,x(:,1));
+    subplot(1, 6, 1);
+    plot(t, x(:, 1));
     xlabel('x_s (offset of body)');
 
-    subplot(1,4,2);
-    plot(t,x(:,2));
+    subplot(1, 6, 2);
+    plot(t, x(:, 2));
     xlabel('x_u (offset of tire)');
 
-    subplot(1,4,3);
-    plot(t,x(:,3));
+    subplot(1, 6, 3);
+    plot(t, x(:, 3));
     xlabel('v_s (velocity of body)');
 
-    subplot(1,4,4);
-    plot(t,x(:,4));
+    subplot(1, 6, 4);
+    plot(t, x(:, 4));
     xlabel('v_u (velocity of tire)');
+
+    subplot(1, 6, 5);
+    plot(tt, F1);
+    xlabel('a_s (acceleration of body)');
+
+    subplot(1, 6, 6);
+    plot(tt, F2);
+    xlabel('a_u (acceleration of tire)');
 end

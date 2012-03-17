71e474a8dfb61142e92ea3bc9342b1aca0a8ed64
Aaron Fineman <abyxcos@gmail.com>
2012-03-17 18:07:21 -0400

    Pull half car matricies into the half car init script

--- init_globals_half.m
+++ init_globals_half.m
@@ -19,5 +19,52 @@ h_car.kt = 200000;
 % Dampers
 h_car.bu = 4800;     % Shocks
 
-% Gravity
-h_car.grav = 9.81;
+% Mass matrix
+h_car.m = [h_car.ms 0 0 0;
+          0 h_car.Ix 0 0;
+          0 0 h_car.m1 0;
+          0 0 0 h_car.m2];
+
+% Damper matrix
+B11 = 2*h_car.bu;
+B12 = h_car.bu*h_car.b1 - h_car.bu*h_car.b2;
+B13 = -h_car.bu;
+B14 = -h_car.bu;
+B21 = h_car.bu*h_car.b1 - h_car.bu*h_car.b2;
+B22 = h_car.bu*h_car.b1^2 + h_car.bu*h_car.b2^2;
+B23 = -h_car.bu*h_car.b1;
+B24 = h_car.bu*h_car.b2;
+B31 = -h_car.bu;
+B32 = -h_car.bu*h_car.b1;
+B33 = h_car.bu;
+B34 = 0;
+B41 = -h_car.bu;
+B42 = h_car.bu*h_car.b2;
+B43 = 0;
+B44 = h_car.bu;
+h_car.b = [B11 B12 B13 B14;
+          B21 B22 B23 B24;
+          B31 B32 B33 B34;
+          B41 B42 B43 B44];
+
+% Spring matrix
+K11 = 2*h_car.ku;
+K12 = h_car.ku*h_car.b1 - h_car.ku*h_car.b2;
+K13 = -h_car.ku;
+K14 = -h_car.ku;
+K21 = h_car.ku*h_car.b1 - h_car.ku*h_car.b2;
+K22 = h_car.ku*h_car.b1^2 + h_car.ku*h_car.b2^2;
+K23 = -h_car.ku*h_car.b1;
+K24 = h_car.ku*h_car.b2;
+K31 = -h_car.ku;
+K32 = -h_car.ku*h_car.b1;
+K33 = h_car.ku + h_car.kt;
+K34 = 0;
+K41 = -h_car.ku;
+K42 = h_car.ku*h_car.b2;
+K43 = 0;
+K44 = h_car.ku + h_car.kt;
+h_car.k = [K11 K12 K13 K14;
+          K21 K22 K23 K24;
+          K31 K32 K33 K34;
+          K41 K42 K43 K44];
--- modelHalf.m
+++ modelHalf.m
@@ -8,65 +8,16 @@
 % Returns:
 %     The xdot for the model in its current state
 function xdot = modelHalf(t, x, p)
-
-    % Mass matrix
-    m = [p.ms 0 0 0;
-        0 p.Ix 0 0;
-        0 0 p.m1 0;
-        0 0 0 p.m2];
-    
-    % Damper matrix
-    B11 = 2*p.bu;
-    B12 = p.bu*p.b1 - p.bu*p.b2;
-    B13 = -p.bu;
-    B14 = -p.bu;
-    B21 = p.bu*p.b1 - p.bu*p.b2;
-    B22 = p.bu*p.b1^2 + p.bu*p.b2^2;
-    B23 = -p.bu*p.b1;
-    B24 = p.bu*p.b2;
-    B31 = -p.bu;
-    B32 = -p.bu*p.b1;
-    B33 = p.bu;
-    B34 = 0;
-    B41 = -p.bu;
-    B42 = p.bu*p.b2;
-    B43 = 0;
-    B44 = p.bu;
-    b = [B11 B12 B13 B14;
-        B21 B22 B23 B24;
-        B31 B32 B33 B34;
-        B41 B42 B43 B44];
-    
-    % Spring matrix
-    K11 = 2*p.ku;
-    K12 = p.ku*p.b1 - p.ku*p.b2;
-    K13 = -p.ku;
-    K14 = -p.ku;
-    K21 = p.ku*p.b1 - p.ku*p.b2;
-    K22 = p.ku*p.b1^2 + p.ku*p.b2^2;
-    K23 = -p.ku*p.b1;
-    K24 = p.ku*p.b2;
-    K31 = -p.ku;
-    K32 = -p.ku*p.b1;
-    K33 = p.ku + p.kt;
-    K34 = 0;
-    K41 = -p.ku;
-    K42 = p.ku*p.b2;
-    K43 = 0;
-    K44 = p.ku + p.kt;
-    k = [K11 K12 K13 K14;
-        K21 K22 K23 K24;
-        K31 K32 K33 K34;
-        K41 K42 K43 K44];
+    global g;
     
     % Input disturbance matrix
     % TODO : Revise to actual model; add Force Function
-    F = [-p.ms*p.grav; 0; p.kt*0; p.kt*0];
+    F = [-p.ms*g; 0; p.kt*disturbance_step(t); p.kt*disturbance_step(t)];
     
     % Pull out position and velocity, calculate acceleration.
     pos = [x(1); x(2); x(3); x(4)];
     vel = [x(5); x(6); x(7); x(8)];
-    accel = m \ (F - b*vel - k*pos);
+    accel = p.m \ (F - p.b*vel - p.k*pos);
     
     % Package the data in the output
     xdot = [0; 0; 0; 0; 0; 0; 0; 0];
